ae2bf0485f87158b84e2c2b66c0f3ce6ec3e3341
Zhukovskiy Stanislav <menistrel2005@yandex.ru>
2017-08-03 19:38:00 +0300

    fixed tasks

--- gulp/tasks/copy.image.js
+++ gulp/tasks/copy.image.js
@@ -3,7 +3,7 @@
 module.exports = function () {
   $.gulp.task('copy:image', function () {
 
-    var images = $.gulp.src('./source/images/generals/**/*.*', {
+    var images = $.gulp.src('./source/images/general/**/*.*', {
         since: $.gulp.lastRun('copy:image')
       })
       .pipe($.gp.imagemin())
--- gulp/tasks/sprite.png.js
+++ gulp/tasks/sprite.png.js
@@ -2,7 +2,7 @@
 
 module.exports = function () {
   $.gulp.task('sprite:png', function () {
-    var spriteData = $.gulp.src(['./source/images/general**/*.png', '!./source/images/general/**/_*.png'])
+    var spriteData = $.gulp.src(['./source/images/general/**/*.png', '!./source/images/general/**/_*.png'])
       .pipe($.gp.spritesmith({
         imgName: 'sprite.png',
         cssName: 'png.sprite.css'

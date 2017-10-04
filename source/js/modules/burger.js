b8ce60612125e3f0f85537d5ee969ec3497aeac2
Zhukovskiy Stanislav <menistrel2005@yandex.ru>
2017-10-04 20:27:03 +0300

    adaptive add index.page

--- source/js/modules/burger.js
+++ source/js/modules/burger.js
@@ -1,21 +1,15 @@
 module.exports = function() {
-        const burger = document.querySelector('.burger');
-        const nav = document.querySelector('#main-nav');
+        const burger = document.querySelector('.btn-burger');
+        const nav = document.querySelector('.nav-header');
         
         burger.onclick = function (e) {
             if (burger.classList.contains('active')) {
                 burger.classList.remove('active');
                 burger.classList.remove('no-animation');
-                nav.style.opacity = '0';
-                setTimeout(function() {
-                   nav.style.left = '-9999px';  
-                }, 500)
             }
             else {
                 burger.classList.add('active');
                 burger.classList.remove('no-animation');
-                nav.style.left = '0'; 
-                nav.style.opacity = '1';   
             }
             return false;
         }
